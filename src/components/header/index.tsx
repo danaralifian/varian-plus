4c93de7a47c28850b802dd3e15a010247c5677d2
danaralifian <danaralifian@gmail.com>
2025-02-26 18:13:08 +0700

    feat: header desktop

---
+++ src/components/header/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className='py-3 px-4 sticky top-0 min-h-[64px] flex items-center '>
+            <div className='container flex items-center justify-between'>
+                <Image src='/logo.svg' alt='logo varian plus' className='w-auto h-8' width={'100'} height={100} />
+                <nav>
+                    <ul className='flex space-x-6 items-center list-none'>
+                        <li><a href='#'>About</a></li>
+                        <li><a href='#'>Our Products</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        sm: "2rem",
+        md: "4rem",
+        lg: "6rem",
+        xl: "5rem",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
