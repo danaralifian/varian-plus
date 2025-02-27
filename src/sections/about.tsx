5950b5562dde4c7dd9de2c042d46c8fd1f9a6f93
danaralifian <danaralifian@gmail.com>
2025-02-27 18:08:17 +0700

    fix: size image about

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <section className='container space-y-6' id='about-us'>
       <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 items-center'>
-        <Image src='/images/business.webp' alt='about' width={500} height={500} className='w-full rounded-md object-cover' />
+        <Image src='/images/business.webp' alt='about' width={760} height={407} className='w-full rounded-md object-cover' />
         <div className='space-y-4'>
           <div className='rounded-full w-fit p-2 px-4 bg-lime-100 mx-auto lg:mx-0'>
             <h2 className='text-sm md:text-lg font-bold w-fit title-gradient'>About Us</h2>
