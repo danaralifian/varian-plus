4cdb3667b31454cbdbf90144093ac746e53b4aea
danaralifian <danaralifian@gmail.com>
2025-02-27 10:32:45 +0700

    feat: drawer nav

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,40 @@
+"use client"
+import MENUS from '@/constants/menus'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
     return (
-        <header className='py-3 px-4 sticky top-0 min-h-[64px] flex items-center bg-white z-10 shadow-md'>
+        <header className='sticky top-0 min-h-[64px] flex items-center bg-white z-10 shadow-md'>
+            {/* Navbar */}
             <div className='container flex items-center justify-between'>
-                <Image src='/logo.svg' alt='logo varian plus' className='w-auto h-8' width={'100'} height={100} />
-                <nav>
+                <Image src='/logo.svg' alt='logo varian plus' className='w-auto h-6 md:h-8' width={'100'} height={100} />
+                <nav className='hidden md:block'>
                     <ul className='flex space-x-6 items-center list-none'>
-                        <li><a href='#' className='font-bold'>About</a></li>
-                        <li><a href='#' className='font-bold'>Our Products</a></li>
+                        {MENUS.map((menu, index) => <li key={index}><a href={menu.href} className='font-bold'>{menu.name}</a></li>)}
                     </ul>
                 </nav>
+                <div className='cursor-pointer block md:hidden' onClick={() => setIsOpen(!isOpen)}>
+                    <Image src='/menu.webp' alt='menu' className='w-6 h-6' width={64} height={64} />
+                </div>
+            </div>
+            {/* Drawer */}
+            <div
+                className={cn('fixed top-0 right-0 h-full w-full inset-0 bg-black/50 z-10 shadow-lg transition-transform transform', {
+                    'translate-x-0': isOpen,
+                    'translate-x-full': !isOpen
+                })}
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <div className='w-full h-full max-w-xs bg-white right-0 absolute p-4 py-5'>
+                    <Image src='/logo.svg' alt='logo varian plus' className='w-auto h-6 md:h-8' width={'100'} height={100} />
+                    <ul className='space-y-2 list-none mt-8'>
+                        {MENUS.map((menu, index) => <li key={index} className='bg-lime-100 p-2 px-4 rounded-md'><a href={menu.href} className='font-bold'>{menu.name}</a></li>)}
+                    </ul>
+                </div>
             </div>
         </header>
     )
---
+++ src/constants/menus.ts
@@ -0,0 +1,12 @@
+const MENUS = [
+  {
+    name: "About Us",
+    href: "#about-us",
+  },
+  {
+    name: "Our Products",
+    href: "#our-products",
+  },
+];
+
+export default MENUS;
