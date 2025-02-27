73c4dc3ad23cdef6b8354ea22351162d91622d64
danaralifian <danaralifian@gmail.com>
2025-02-27 15:29:29 +0700

    fix: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,14 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     container: {
       center: true,
       padding: {
         DEFAULT: "1rem",
-        sm: "o0rem",
+        sm: "0rem",
       },
     },
     keyframes: {
