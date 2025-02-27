f319b95a6c710a541f2cf22aa18b1135011e952e
danaralifian <danaralifian@gmail.com>
2025-02-27 15:06:30 +0700

    fix: transition drawer

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -40,13 +40,15 @@ export default function Header() {
             </div>
             {/* Drawer */}
             <div
-                className={cn('fixed top-0 right-0 h-full w-full inset-0 bg-black/50 z-10 shadow-lg transition-transform transform', {
-                    'translate-x-0': isOpen,
-                    'translate-x-full': !isOpen
+                className={cn('fixed top-0 right-0 h-full w-full inset-0 bg-black/50 z-10 shadow-lg opacity-0 pointer-events-none duration-300', {
+                    'opacity-100 pointer-events-auto': isOpen
                 })}
                 onClick={() => setIsOpen(!isOpen)}
             >
-                <div className='w-full h-full max-w-xs bg-white right-0 absolute p-4 py-5'>
+                <div className={cn('w-full h-full max-w-xs bg-white right-0 absolute p-4 py-5 transition-transform transform duration-300 ease-in-out', {
+                    'translate-x-0': isOpen,
+                    'translate-x-full': !isOpen
+                })}>
                     <Image src='/logo.svg' alt='logo varian plus' className='w-auto h-6 md:h-8' width={'100'} height={100} />
                     <ul className='space-y-2 list-none mt-8'>
                         {MENUS.map((menu, index) => <li key={index} className='bg-lime-100 p-2 px-4 rounded-md cursor-pointer font-bold' onClick={() => handleClickMenu(menu.href)}>{menu.name}</li>)}
